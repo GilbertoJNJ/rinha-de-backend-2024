2951659cc5474573367e5a3b484b9f7c57f104f5
Gilberto Júnior <gilbertojnj@gmail.com>
2024-03-10 00:40:35 -0300

    Corrige tratamento de erros

--- src/main/java/com/gilberto/rinhadebackend/controllers/CustomerController.java
+++ src/main/java/com/gilberto/rinhadebackend/controllers/CustomerController.java
@@ -1,6 +1,8 @@
 package com.gilberto.rinhadebackend.controllers;
 
 import com.gilberto.rinhadebackend.exceptions.CustomerNotFoundException;
+import com.gilberto.rinhadebackend.exceptions.InconsistentBalanceException;
+import com.gilberto.rinhadebackend.exceptions.UnsupportedModalityException;
 import com.gilberto.rinhadebackend.models.requests.TransactionForm;
 import com.gilberto.rinhadebackend.services.ICustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,10 +27,16 @@ public class CustomerController {
   
   @PostMapping("/{id}/transacoes")
   public ResponseEntity<Object> transacts(@PathVariable Long id,
-                                                  @RequestBody TransactionForm form) {
+                                          @RequestBody TransactionForm form) {
     try {
       return ResponseEntity.ok(customerService.transacts(id, form));
     } catch (CustomerNotFoundException exception) {
+      return ResponseEntity.status(exception.getStatus())
+          .body(exception.getMessage());
+    } catch (InconsistentBalanceException exception) {
+      return ResponseEntity.status(exception.getStatus())
+          .body(exception.getMessage());
+    } catch (UnsupportedModalityException exception) {
       return ResponseEntity.status(exception.getStatus())
           .body(exception.getMessage());
     }
@@ -43,5 +51,5 @@ public class CustomerController {
           .body(exception.getMessage());
     }
   }
-
+  
 }
